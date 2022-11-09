20da0e5f2ef1ca5800f3b5215d9baf4a82c2b8a0
jp-dev2 <117818542+jp-dev2@users.noreply.github.com>
2022-11-09 11:32:01 +0100

    Update streamlit_app.py
    Added pandas dataframe and AWS data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -10,4 +11,6 @@ streamlit.text('Avocado Toast')
 
 streamlit.header('Build Your Own Smoothie')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+streamlit.dataframe(my_fruit_list)
