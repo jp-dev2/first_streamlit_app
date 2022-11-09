92d511e3a1c2878a0a402c949d2379d15b22a8cc
jp-dev2 <117818542+jp-dev2@users.noreply.github.com>
2022-11-09 11:24:06 +0100

    Update streamlit_app.py
    Added New Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,8 @@ streamlit.header('Breakfast Favouties')
 streamlit.text('🫐Omega 3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard-Bioiled Free-Range Egg')
+streamlit.text('Avocado Toast')
+
+streamlit.header('Build You Own Smoothie')
 
 
